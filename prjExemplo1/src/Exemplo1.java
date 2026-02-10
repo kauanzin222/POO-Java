e818a2d35f143e88f8227799dbf0650f782c7c21
Kauã Cardoso <kauaanthony.cardoso44@gmail.com>
2026-02-10 10:54:55 -0300

    Add files via upload

---
+++ prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author 0030482421022
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Linguagem Java");
+    }
+
+}
