c0d7450f0bcac44f6a4b483136676290e542c8c2
Kauã Cardoso <kauaanthony.cardoso44@gmail.com>
2026-03-25 10:19:05 -0300

    Update App.java

--- ExercicioPilares/prjExercicioGenerEspecComPolimorfismo/src/App.java
+++ ExercicioPilares/prjExercicioGenerEspecComPolimorfismo/src/App.java
@@ -1,5 +1,4 @@
 
-import fatec.poo.model.Funcionario;
 import fatec.poo.model.FuncionarioComissionado;
 import fatec.poo.model.FuncionarioHorista;
 import fatec.poo.model.FuncionarioMensalista;
@@ -18,13 +17,14 @@ public class App {
         funcHor.setCargo("Estágiario");
 
         System.out.println("---\tFUNCIONÁRIO HORISTA");
-        System.out.println("Nome:" + funcHor.getNome());
-        System.out.println("Data de Admissão: " + funcHor.getDtAdmissao().format(fmt1));
-        System.out.println("Cargo: " + funcHor.getCargo());
-        System.out.printf("Salário Bruto: %.2f\n", funcHor.calcSalBruto());
-        System.out.printf("Desconto: %.2f\n", funcHor.calcDesconto());
-        System.out.printf("Salário Líquido: %.2f\n", funcHor.calcSalLiquido());
-        System.out.printf("Gratificação: %.2f\n", funcHor.calcGratificacao());
+        System.out.println("Nome:                => " + funcHor.getNome());
+        System.out.println("Data de Admissão:    => " + funcHor.getDtAdmissao().format(fmt1));
+        System.out.println("Cargo:               => " + funcHor.getCargo());
+        System.out.printf("Salário Bruto:       => R$ %.2f\n", funcHor.calcSalBruto());
+        System.out.printf("Desconto:            => R$ %.2f\n", funcHor.calcDesconto());
+        System.out.printf("Salário Líquido:     => R$ %.2f\n", funcHor.calcSalLiquido());
+        System.out.printf("Gratificação:        => R$ %.2f\n", funcHor.calcGratificacao());
+        
 
         FuncionarioMensalista funcMens = new FuncionarioMensalista(1011, "Maria Antonieta",
                 LocalDate.parse("21/03/1980", fmt1), 7.5);
@@ -33,12 +33,12 @@ public class App {
         funcMens.setCargo("Desenvolvedor Sênior");
 
         System.out.println("\n---\tFUNCIONÁRIO MENSALISTA");
-        System.out.println("Nome: " + funcMens.getNome());
-        System.out.println("Data de Admissão: " + funcMens.getDtAdmissao().format(fmt1));
-        System.out.println("Cargo: " + funcMens.getCargo());
-        System.out.printf("Salário Bruto: %.2f\n", funcMens.calcSalBruto());
-        System.out.printf("Desconto: %.2f\n", funcMens.calcDesconto());
-        System.out.printf("Salário Líquido: %.2f\n", funcMens.calcSalLiquido());
+        System.out.println("Nome:                    => " + funcMens.getNome());
+        System.out.println("Data de Admissão:        => " + funcMens.getDtAdmissao().format(fmt1));
+        System.out.println("Cargo:                   => " + funcMens.getCargo());
+        System.out.printf("Salário Bruto:           => R$ %.2f\n", funcMens.calcSalBruto());
+        System.out.printf("Desconto:                => R$ %.2f\n", funcMens.calcDesconto());
+        System.out.printf("Salário Líquido:         => R$%.2f\n", funcMens.calcSalLiquido());
 
         FuncionarioComissionado funcCom = new FuncionarioComissionado(1012, "Marcos Vieira",
                 LocalDate.parse("10/02/2000", fmt1), 0.2);
@@ -47,13 +47,13 @@ public class App {
         funcCom.addVendas(5001.0);
 
         System.out.println("\n---\tFUNCIONÁRIO COMISSIONADO");
-        System.out.println("Nome: " + funcCom.getNome());
-        System.out.println("Data de Admissão: " + funcCom.getDtAdmissao().format(fmt1));
-        System.out.println("Cargo: " + funcCom.getCargo());
-        System.out.printf("Salário Bruto: %.2f\n", funcCom.calcSalBruto());
-        System.out.printf("Desconto: %.2f\n", funcCom.calcDesconto());
-        System.out.printf("Salário Líquido: %.2f\n", funcCom.calcSalLiquido());
-        System.out.printf("Gratificação: %.2f\n", funcCom.calcGratificacao());
+        System.out.println("Nome:                    => " + funcCom.getNome());
+        System.out.println("Data de Admissão:        => " + funcCom.getDtAdmissao().format(fmt1));
+        System.out.println("Cargo:                   => " + funcCom.getCargo());
+        System.out.printf("Salário Bruto:           => R$ %.2f\n", funcCom.calcSalBruto());
+        System.out.printf("Desconto:                => R$ %.2f\n", funcCom.calcDesconto());
+        System.out.printf("Salário Líquido:         => R$ %.2f\n", funcCom.calcSalLiquido());
+        System.out.printf("Gratificação:            => R$ %.2f\n", funcCom.calcGratificacao());
     }
 
 }
